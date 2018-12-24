f7f520e7fa2d299305c79f0a00e5121cc9d38732
Unknown <markypizz@gmail.com>
2018-12-24 17:24:26 -0500

    New files

---
+++ ChessTests/ChessTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChessTests.swift
+//  ChessTests
+//
+//  Created by Mark Pizzutillo on 12/24/18.
+//  Copyright © 2018 Mark Pizzutillo. All rights reserved.
+//
+
+import XCTest
+@testable import Chess
+
+class ChessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
